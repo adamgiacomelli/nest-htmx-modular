874ef190762950b915f4e955c526f64c5afa0ef9
Adam Giacomelli <adam.giacom@gmail.com>
2024-10-16 09:48:58 +0200

    Ejs templates added, controller updated, mock service added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+
+enum Pages {
+  DASHBOARD,
+  USERS
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  async index(
+  ) {
+    return {
+      current_page: Pages.DASHBOARD
+    } 
+  }
+
+  @Get('dashboard')
+  @Render('dashboard')
+  async dashboard(
+  ) {
+    let user_count = await this.appService.getUserCount()
+    return {
+      user_count
+    } 
+  }
+
+  @Get('users')
+  @Render('users')
+  async users(
+  ) {
+    let users = await this.appService.getUsers()
+    return {
+      users
+    } 
   }
 }
+
+
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
+// To avoid setting up the database we set up some static user data for demonstration purposes
+const users = [
+  { name: 'User1', email: 'user1@nonexisting.com' },
+  { name: 'User2', email: 'user2@nonexisting.com' },
+  { name: 'User3', email: 'user3@nonexisting.com' },
+  { name: 'User4', email: 'user4@nonexisting.com' },
+  { name: 'User5', email: 'user5@nonexisting.com' },
+];
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getUsers() {
+    return users;
+  }
+
+  async getUserCount() {
+    return users.length
   }
 }
